f01938d3166eede65640fc19f4ceddca63bd9b6d
jettizz1 <khomsanopen@gmail.com>
2026-08-20 22:22:54 +0700

    new update

---
+++ labs/week-05/lab05/starter/src/App.jsx
@@ -0,0 +1,26 @@
+import { Route, Routes } from 'react-router-dom';
+import AppLayout from './pages/AppLayout.jsx';
+
+import AboutPage from './pages/AboutPage.jsx';
+import DashboardPage from './pages/DashboardPage.jsx';
+import NewRequestPage from './pages/NewRequestPage.jsx';
+import NotFoundPage from './pages/NotFoundPage.jsx';
+import RequestDetailPage from './pages/RequestDetailPage.jsx';
+
+function App() {
+  return (
+    <Routes>
+     <Route element={<AppLayout />}>
+        <Route index element={<DashboardPage />} />
+        <Route element={<NewRequestPage />} path="requests/new" />
+        <Route element={<RequestDetailPage />} path="requests/:requestId" />
+        <Route element={<AboutPage />} path="about" />
+        <Route element={<NotFoundPage />} path="*" />
+      </Route>
+    </Routes>
+
+
+  );
+  // TODO 5A-CP-02: เปลี่ยนเป็น <Routes> ที่มี AppLayout เป็นกรอบ
+}
+export default App;
